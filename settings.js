e0aab83ce57e4c7e68dc3f025719e33889fba80c
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2022-11-07 21:42:40 -0300

    up

---
+++ settings.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = 'lPurNi4wLG09ECaQDIkivaRP2HcluA7ObNssJrnW';
+var REDIRECT_URI = 'http://localhost:5500/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
